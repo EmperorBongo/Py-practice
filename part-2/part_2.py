647f06de8300b2e2b797169e7c732da579a4de30
EmperorBongo <150077517+EmperorBongo@users.noreply.github.com>
2024-01-22 13:28:09 -0700

    final commit

--- part-2/part_2.py
+++ part-2/part_2.py
@@ -1,29 +1,36 @@
 ### Step 1 - Lists ###
 
 # Fill in this list with several authors you are a fan of. At least 7 or 8 should do.
-my_authors = []
+my_authors = ["Bronson", "Steve", "Jayson", " Anthony", "Amy", "John", "Kim"]
 
 # Now let's add a new author to the end with the .append() method. Type your code below.
 
 # Code here
+my_authors.append("Bronson")
+print(my_authors)
 # Example: my_authors.append("H.G. Wells")
 
 
 # Now let's remove an element in the list
 
 # Code here
+my_authors.remove("Mark twain")
+print(my_authors)
 # Example: my_authors.remove("H.G. Wells")
 
 
 # Now access an element by it's index. (Remember it indexes start at 0.)
 
 # Code here
+print(my_authors[2])
+
 # Example: my_authors[2]
 
 
 # Now slice the list.
 
 # Code here
+print(my_authors[2:5])
 # Example: my_authors[1:4]
 
 
@@ -32,6 +39,7 @@ my_authors = []
 # Create your tuple below. Assign it to a variable name you can reference later in the exercise.
 
 # Code here
+my_author_tuple = ("Bronson", "Steve", "Jayson", " Anthony", "Amy", "John", "Kim")
 # Example: my_author_tuple = ("F. Scott Fitzgerald", "J.K. Rowling", "Ernest Hemingway", "Emily Dickenson", "George Orwell", "Ray Bradbury")
 
 
@@ -40,18 +48,27 @@ my_authors = []
 # Create a set with your authors.
 
 # Code here
+
+my_author_set = {"Bronson", "Steve", "Jayson", " Anthony", "Amy", "John", "Kim"}
+
 # Example: my_author_set = {"F. Scott Fitzgerald", "J.K. Rowling", "Ernest Hemingway", "Emily Dickenson", "George Orwell", "Ray Bradbury"}
 
 
 # Add a new author to your set.
 
 # Code here
+my_author_set.add = ("King Bronson")
+print(my_author_set)
 # Example: my_author_set.add("J.R.R. Tolkien")
 
 
 # Try adding the same author again, and be sure to print your set.
 
 # Code here
+
+my_author_set.add = ("King Bronson")
+print(my_author_set)
+
 # Example: my_author_set.add("J.R.R. Tolkien")
 
 
@@ -72,3 +89,11 @@ my_authors = []
 # for book in my_authors_set:
     # print(book)
 
+for book in my_authors:
+    print(book)
+
+for book in my_author_tuple:
+    print(book)
+
+for book in my_author_set:
+    print(book)
